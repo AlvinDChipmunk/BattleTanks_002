06765e7bcdfd2db00f29ba6d32125f4cb228fee0
ALVIN750LAPTOP\Alvin <alvinrevilas@yahoo.com>
2017-09-21 22:32:51 -0700

    Lesson 104 - Creating & Deleting Landscapes

---
+++ BattleTanks_002/Source/BattleTanks_002/BattleTanks_002GameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTanks_002.h"
+#include "BattleTanks_002GameMode.h"
+
+
+
+
---
+++ BattleTanks_002/Source/BattleTanks_002/BattleTanks_002GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BattleTanks_002GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKS_002_API ABattleTanks_002GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
