06562015c66a1470c6a893cd70eae4db7fb82564
ALVIN750LAPTOP\Alvin <alvinrevilas@yahoo.com>
2017-10-17 18:11:36 -0700

    17 Oct 2017 - Finished lesson 140 - BlueprintSpawnableComponent() - Can now make C++ objects appear in Blueprint and can have data elements become visible or not depending on C++ source code.

--- BattleTanks_002/Source/BattleTanks_002/Public/TankBarrelMesh.h
+++ BattleTanks_002/Source/BattleTanks_002/Public/TankBarrelMesh.h
@@ -8,7 +8,8 @@
 /**
  * 
  */
-UCLASS()
+UCLASS( meta = (BlueprintSpawnableComponent), hidecategories = ("Collision") )
+//UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BATTLETANKS_002_API UTankBarrelMesh : public UStaticMeshComponent
 {
 	GENERATED_BODY()
@@ -20,11 +21,23 @@ protected:
 
 
 private: 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, Category = Setup)
 		float AbsoluteElevationSpeed = 4.0f;
 	
-	float MaxPosElevationSpeed = +1.0 * AbsoluteElevationSpeed; 
+	//How far does the barrel elevate up? 
+	UPROPERTY(EditAnywhere, Category = Setup)
+		float MaxElevationAngle = 60.0f;
 
-	float MaxNegElevationSpeed = -1.0 * AbsoluteElevationSpeed;
+	//How fast does the barrel elevate up? 
+	UPROPERTY(EditAnywhere, Category = Setup)
+		float MaxPosElevationSpeed = +1.0 * AbsoluteElevationSpeed;
+
+	//How far does the barrel elevate down?
+	UPROPERTY(EditAnywhere, Category = Setup)
+		float MinElevationAngle = 0.0f;
+
+	//How fast does the barrel elevate down?
+	UPROPERTY(EditAnywhere, Category = Setup)
+		float MaxNegElevationSpeed = -1.0 * AbsoluteElevationSpeed;
 
 };
